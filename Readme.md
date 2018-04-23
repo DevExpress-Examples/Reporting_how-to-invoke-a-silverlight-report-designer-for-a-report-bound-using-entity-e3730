a27ff74420db1fe2358ec94b752204f453b2163d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to invoke a Silverlight Report Designer for a report bound using Entity Framework
+
+
+<p>This sample shows how you can provide an in-browser Report Designer in your Silverlight application for editing reports that are bound using Entity Framework.</p><p>See also:<br />
+- <a href="https://www.devexpress.com/Support/Center/p/E3690">How to use the Silverlight report designer</a>;<br />
+- <a href="https://www.devexpress.com/Support/Center/p/E3729">How to invoke a Silverlight Report Designer for a report bound to a dataset</a>;<br />
+- <a href="https://www.devexpress.com/Support/Center/p/E3731">How to invoke a Silverlight Report Designer for a report bound using XPO</a>.</p>
+
+<br/>
+
+
---
+++ VB/ReportsSample.Web/ReportService.svc.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Forms
+Imports DevExpress.Data.Utils.ServiceModel
+Imports DevExpress.XtraReports.Service
+Imports DevExpress.XtraReports.UI
+Imports ReportLibrary
+
+Namespace ReportsSample.Web
+	<SilverlightFaultBehavior> _
+	Public Class ReportService
+		Inherits DevExpress.XtraReports.Service.ReportService
+		Protected Overrides Sub RegisterDataSources(ByVal report As XtraReport, ByVal reportName As String)
+			report.RegisterDataSourceName("Products", report.DataSource)
+		End Sub
+		Protected Overrides Sub FillDataSources(ByVal report As XtraReport, ByVal reportName As String, ByVal isDesignActive As Boolean)
+			Dim bindingSource = CType(report.GetDataSourceByName("Products"), BindingSource)
+			bindingSource.DataSource = New NorthwindEntities().Products
+		End Sub
+	End Class
+End Namespace
